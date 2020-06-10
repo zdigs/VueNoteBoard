5b36eef0fe0fc9cf511a97cefbddc4a9079831fe
Brandon M. Scrushy <brandon@zcorp.digital>
2020-06-10 13:18:58 -0500

    Added static folder n stuff

--- server/index.js
+++ server/index.js
@@ -5,14 +5,22 @@ const cors = require('cors');
 const app = express();
 
 //Middleware
-
-// app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
 
 const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
+
+// Handle Production
+
+if(process.env.NODE_ENV === 'production'){
+    // Static Folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle SPA
+    app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 5000;
 
